144091931446bdbc13a08571b6d402f3c9f6ed52
moshang <moshang@atguigu.com>
2021-10-18 05:36:01 +0800

    idea first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author moshang
+ * @create 2021-10-18 5:31
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
